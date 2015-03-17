c19bd485fc77544d493619ce1f32b22995bf78d4
Richard Tanner <dicktanner@gmail.com>
2015-03-16 18:39:26 -0700

    Set wordnik to https

--- app/scripts/services/word.js
+++ app/scripts/services/word.js
@@ -1,13 +1,13 @@
 angular.module('opendoorApp')
 .service('WordService', [ '$http', function ($http) {
   //Get initial word
-  var url = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=proper-noun&minCorpusCount=15000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=4&maxLength=6&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+  var url = "https://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=proper-noun&minCorpusCount=15000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=4&maxLength=6&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
   var get = function(){
     return $http.get(url);
   };
 
   //Check for word in case user recombines word in new way
-  var checkUrlBase = "http://api.wordnik.com:80/v4/word.json/";
+  var checkUrlBase = "https://api.wordnik.com:80/v4/word.json/";
   var checkUrlEnd = "/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
   var check = function(word){
     word = word.toLowerCase();
